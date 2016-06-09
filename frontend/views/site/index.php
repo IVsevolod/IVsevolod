2742bf56f8d4803b9d619192cca539b2beefac15
IVsevolod <v.ivanov@zavento.com>
2016-06-09 14:07:41 +0300

    Контакты.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,10 +2,46 @@
 
 /* @var $this yii\web\View */
 
+$this->registerCssFile('pe-icon-social/css/pe-icon-social.css');
+$this->registerCssFile('pe-icon-social/css/helper.css');
+$this->registerCssFile('pe-icon-social/css/social-style.css');
+
 $this->title = 'IVsevolod';
 ?>
 <div class="site-index">
     <div class="col-md-6">
         <h3>Привет, друзья!</h3>
+        <p>
+            Рад приветствовать вас на своем сайте.<br/>
+            Меня зовут Всеволод и я - Человек.<br/>
+            Я искренне люблю свою жену, семью, веб-разработку, танцы, море и горы.
+        </p>
+        <h3>Как связаться и где меня найти</h3>
+        <ul class="contacts">
+            <li>
+                <span class="pe-so-skype"></span> IVsevolod
+            </li>
+            <li>
+                <a href="https://ru.linkedin.com/in/ivsevolod">
+                    <span class="pe-so-linkedin"></span> Vsevolod Ivanov в LinkedIn
+                </a>
+            </li>
+            <li>
+                <a href="https://new.vk.com/id2107981">
+                    <span class="pe-so-vk"></span> Всеволод Иванов Вконтакте
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/IVsevolod">
+                    <span class="pe-so-facebook"></span> Vsevolod Ivanov на Facebook
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/IVsevolod">
+                    <span class="pe-so-github"></span> Vsevolod на Github
+                </a>
+            </li>
+        </ul>
+
     </div>
 </div>
