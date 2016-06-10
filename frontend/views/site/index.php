bcc88e644b2af21beaae5f04d9fa035aaa3e0e9e
IVsevolod <v.ivanov@zavento.com>
2016-06-10 13:54:05 +0300

    Добавленаы проекты.

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -16,6 +16,7 @@ $this->title = 'IVsevolod';
             Меня зовут Всеволод и я - Человек.<br/>
             Я искренне люблю свою жену, семью, веб-разработку, танцы, море и горы.
         </p>
+
         <h3>Как связаться и где меня найти</h3>
         <ul class="contacts">
             <li>
@@ -27,7 +28,7 @@ $this->title = 'IVsevolod';
                 </a>
             </li>
             <li>
-                <a href="https://new.vk.com/id2107981">
+                <a href="https://vk.com/id2107981">
                     <span class="pe-so-vk"></span> Всеволод Иванов Вконтакте
                 </a>
             </li>
@@ -43,5 +44,74 @@ $this->title = 'IVsevolod';
             </li>
         </ul>
 
+        <h3>Мои группы, каналы и другое:</h3>
+        <ul class="contacts">
+            <li>
+                <a href="https://vk.com/my_home_happy">
+                    <span class="pe-so-vk"></span> Группа Вконтакте для Души
+                </a>
+            </li>
+            <li>
+                <a href="https://vk.com/prozouk">
+                    <span class="pe-so-vk"></span> Группа Вконтакте про танец Zouk
+                </a>
+            </li>
+            <li>
+                <a href="https://vk.com/snakebattle">
+                    <span class="pe-so-vk"></span> Группа Вконтакте битвы змей
+                </a>
+            </li>
+            <li>
+                <a href="https://www.youtube.com/channel/UCTDPXDsQqdMEmQ4aidSDomQ">
+                    <span class="pe-so-youtube-1"></span> Канал в Youtube про танец Zouk
+                </a>
+            </li>
+            <li>
+                <a href="https://plus.google.com/+BrazilianzoukRuStyle">
+                    <span class="pe-so-google-plus"></span> GooglePlus про танец Zouk
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-md-6">
+        <h3>Проекты</h3>
+        <table class="projects-preview-block">
+            <tr>
+                <td>
+                    <a href="http://battlesnake.ru/">
+                        <img src="img/project/battlesnake.png">
+                    </a>
+                </td>
+                <td>
+                    <a href="http://battlesnake.ru/">battlesnake.ru</a> - <b>Битва змей.</b><br/>
+                    <b>Дата создания:</b> 24.08.2013<br/>
+                    <b>Framework:</b> Yii
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="http://tavan-en.ru/">
+                        <img src="img/project/tavan-en.png">
+                    </a>
+                </td>
+                <td>
+                    <a href="http://tavan-en.ru/">tavan-en.ru</a> - <b>Газета.</b><br/>
+                    <b>Дата создания:</b> 02.07.2012<br/>
+                    <b>CMS:</b> WP
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="http://brazilianzouk.ru/">
+                        <img src="img/project/prozouk.png">
+                    </a>
+                </td>
+                <td>
+                    <a href="http://brazilianzouk.ru/">brazilianzouk.ru</a> - <b>Про танец Zouk.</b><br/>
+                    <b>Дата создания:</b> 06.03.2016<br/>
+                    <b>Framework:</b> Yii2
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
