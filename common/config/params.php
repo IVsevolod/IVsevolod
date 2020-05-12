590b8829da234919857cf34823967745766efe6e
ivsevolod <vsevolod.ivanov@click2mice.com>
2020-05-12 17:48:19 +0300

    Настройка почт

--- common/config/params.php
+++ common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'adminEmail' => 'noreply@ivsevolod.ru',
+    'supportEmail' => 'noreply@ivsevolod.ru',
     'user.passwordResetTokenExpire' => 3600,
 ];
